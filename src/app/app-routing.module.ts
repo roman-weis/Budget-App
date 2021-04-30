0fdc24aa6773eac73ac1b909e33e346a6606853e
Roman Weis <rweis@axioconcept.com>
2021-04-30 13:45:00 +0200

    Add toolbar and display section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component: MainPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
